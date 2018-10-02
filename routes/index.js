6e309074828a88af95b656b87044f900243cddf1
KatherinePetrova <zetsu9@mail.ru>
2018-10-02 11:01:38 +0600

    queries part1

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,104 @@
 var express = require('express');
 var router = express.Router();
-var con = require('../models/connection');
-var query = require('node-mysql-ejq');
-const WebSocket = require('ws');
+
+//Модуль для создания и работы с токенами
 var jwt = require('jsonwebtoken');
 var secret = 'secret';
 
+//Соединение с mysql
+var con = require('../models/connection');
+
+//Модуль для работы с mysql запросами
+var query = require('node-mysql-ejq');
 var q = new query(con);
+
+//Создание WebSocket сервера
+const WebSocket = require('ws');
 const port = 8080;
 const wss = new WebSocket.Server({ port: port });
 console.log('webSocket is listening on port ' + port);
 
+//Массив для хранения соединений с клиентом WebSocket
 var wsCons = [];
 
-//websocket
+//Функция для добавления соединения в массив
 wss.on('connection', function connection(ws) {
 	console.log("Operator connected");
 	wsCons.push(ws);
 });
 
+//Хэдеры для доступа с других портов
 router.use(function(req, res, next) {
  	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  	next();
-}); 
-/* GET home page. */
+});
+
+//Домашняя страница сервера
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Server status: on' });
 });
 
-router.post('/new_driver', async function(req, res, next){
+//Добавление нового водителя
+router.post('/new/driver', async function(req, res, next){
+	//Данные с формы
 	var driver = {
 		name: req.body.name,
-		telegram_id: req.body.userId,
-		phone: req.body.phonenumber,
-		status: req.body.status
+		telegram_id: req.body.id,
+		phone: req.body.phone,
+		car: req.body.car,
+		car_number: req.body.car_number
 	};
 
-	res.send(JSON.stringify(driver));
-	console.log(driver);
-});
+	try{
+		//Отправка данных в базу
+		var insert = await q.insert({table: 'driver', data: driver});
 
-router.post('/new_app', async function(req, res, next){
-
-	var app = {
-		adress: "вс",
-		area: 1,
-		status: 1
-	};
-
-	try {
-		// var insert = await q.insert({table: 'app', data: app});
-		var select = await q.select({table: 'app', keys: ['adress', 'app_cometime'], where: {id: 1}, join: [{table: 'app_status', on: {status: "id"}, keys: ['name']}]});
+		//Получение данных с базы
+		var select = await q.select({table: 'driver', where: {id: insert.insertId}});
 		select = select[0];
-		for(var i=0; i<wsCons.length; i++){
+
+		//Отправление сведений о новом водителе операторам (с помощью WebSocket)
+		for(var i; i<wsCons.length; i++){
+			//Проверка на существование соединения с клиентом
 			try{
-				wsCons[i].send(JSON.stringify(select));
+				wsCons[i].send(JSON.stringify({action: 'new_driver', data: select}));
 			} catch(e){
 				wsCons.splice(i, 1);
 			}
 		}
-		console.log(select);
 
+		//Отправка успеха клиенту
+		res.send();
+
+	} catch(e){
+		console.log(e);
+		//Отправка ошибки клиенту
+		res.send(500).send();
+	}
+});
+
+//Новая заявка
+router.post('/new/app', async function(req, res, next){
+
+	//Данные с формы, статус по умолчанию - новая заявка
+	var app = {
+		adress: req.body.adress,
+		area: req.body.area,
+		status: 1
+	};
+
+	try {
+		var insert = await q.insert({table: 'app', data: app});
+		var select = await q.select({table: 'app', keys: ['id', 'adress', 'app_cometime'], where: {id: 1}, join: [{table: 'app_status', on: {status: "id"}, keys: ['name']}]});
+		select = select[0];
+		for(var i=0; i<wsCons.length; i++){
+			try{
+				wsCons[i].send(JSON.stringify({action: 'new_app', data: select}));
+			} catch(e){
+				wsCons.splice(i, 1);
+			}
+		}
 		res.send();
 	} catch(e) {
 		throw new Error(e)
@@ -70,17 +107,74 @@ router.post('/new_app', async function(req, res, next){
 
 });
 
-router.post('/update_driver', async function(req, res, next){
+//Изменение статуса водителя
+router.post('/update/driver/status', async function(req, res, next){
+	var id = req.body.id;
 	var driver = {
-		name: req.body.name,
-		telegram_id: req.body.userId,
-		phone: req.body.phonenumber,
 		status: req.body.status
 	};
 
-	res.send(JSON.stringify(driver));
-	console.log(driver);
+	try{
+		var update = await q.update({table: 'driver', data: driver, where: {id: id}});
+		for(var i=0; i<wsCons.length; i++){
+			try{
+				wsCons[i].send(JSON.stringify({action: 'update_driver_status', data: {id: id, status: driver.status}}));
+			} catch(e){
+				wsCons.splice(i, 1);
+			}
+		}
+		res.send();
+	} catch(e){
+		throw new Error(e);
+		res.status(500).send();
+	}
 });
+
+//Изменение данных водителя
+router.post('/update/driver/data', async function(req, res, next){
+	var id = req.body.id;
+	var driver = {
+		name: req.body.id,
+		telegram_id: req.body.t_id,
+		phone: req.body.phone
+	};
+
+	try{
+		var update = await q.update({table: 'driver', data: driver, where: {id: id}});
+		for(var i=0; i<wsCons.length; i++){
+			try{
+				wsCons[i].send(JSON.stringify({action: 'update_driver_data', data: {id: id, name: driver.name, phone: driver.phone}}));
+			} catch(e){
+				wsCons.splice(i, 1);
+			}
+		}
+		res.send();
+	} catch(e){
+		throw new Error(e);
+		res.status(500).send();
+	}
+});
+
+//Удаление водителя
+router.post('/delete/driver', async function(req, res, next){
+	var id = req.body.id;
+	try{
+		var del = await q.delete({table: 'driver', where: {telegram_id: id}});
+		for(var i=0; i<wsCons.length; i++){
+			try{
+				wsCons[i].send(JSON.stringify({action: 'delete_driver', data: {telegram_id: id}}));
+			} catch(e){
+				wsCons.splice(i, 1);
+			}
+		}
+		res.send();
+	} catch(e){
+		throw new Error(e);
+		res.status(500).send();
+	}
+});
+
+//Измнение статуса
 
 router.post('/get/new_app', async function(req, res, next){
 	try{
@@ -92,17 +186,5 @@ router.post('/get/new_app', async function(req, res, next){
 	}
 	
 });
-
-router.post('/test/ws', function(req, res, next){
-	try{
-		for(var i=0; i<wsCons.length; i++){
-			wsCons[i].send('Привет');
-		}
-		res.send();
-	} catch(e){
-		res.status(500).send();
-		console.log(e);
-	}
-})
 
 module.exports = router;
