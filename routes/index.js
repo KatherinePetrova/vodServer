c9e6a20668f35ffa6bcad2c7864b7902f647212f
KatherinePetrova <zetsu9@mail.ru>
2018-10-05 12:30:58 +0600

    queries part6.3(errors p3)

--- routes/index.js
+++ routes/index.js
@@ -247,12 +247,12 @@ router.post('/update/app/sent_dec', async function(req, res){
 	var driver = 0;
 	try{
 		var select = q.select({table: 'driver', where: {telegram_id: req.body.telegram_id}, keys: ['id']});
-		console.log('pop');
+		console.log(select);
 		driver = select[0].id;
 		if(typeof driver == 'undefined'){
 			res.status(401).sent();
 		} else {
-			var update = q.update({table: 'driver', where: {id: driver}, data: {status: 1}});
+			var update = q.update({table: 'driver', where: {id: driver}, data: {status: true}});
 			console.log('pop2');
 			for(var i=0; i<wsCons.length; i++){
 				try{
